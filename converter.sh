2315e16594f35dba4860717a4b1c46ab746d256e
toteabe <toteabe@yahoo.es>
2025-10-07 17:14:10 +0200

    pre-warp

--- converter.sh
+++ converter.sh
@@ -5,6 +5,7 @@ IFS=$(echo -en "\n\b")
 cd /markdown-data
 useradd user
 #cat /etc/passwd
+dirbase=$(pwd)
 
 DIRS=""
 find . -type d | while read -r dir; do    
@@ -17,7 +18,8 @@ find . -type d | while read -r dir; do
         /converter-utils/pandoc --template=GitHub.html5 --embed-resources --standalone \
         -f markdown -t html5 --variable=toc-title=Contenido --toc=true -o "${FILENAME}.html" "$i"
     done
-
+    
+    cd $dirbase
 done
 
 chown -R 1000:1000 .
